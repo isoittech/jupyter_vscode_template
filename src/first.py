8bc78275421f33514b5b607ec813028a36047317
t.i <isoittech@gmail.com>
2020-11-07 19:01:18 +0900

    irisの解析・グラフ表示サンプル追加

--- src/first.py
+++ src/first.py
@@ -1,5 +1,15 @@
 #%%
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 #%%
-print("aaaa")
-
+# Pandasのデータフレームとしてデータをロード
+df = pd.read_csv("../data/iris.csv" )
+df.head()
+# %%
+# 種別ごとのSepalLengthのヒストグラム
+# Nameのの種別ごとに、SepalLengthのヒストグラムを描画する
+# sharex、shareyは各ヒストグラムのx、y軸は同じスケールを扱うことを示す
+df.hist(by="species", column="sepal_length", sharex=True, sharey=True)
+#%%
+sns.pairplot(df, hue='species')
